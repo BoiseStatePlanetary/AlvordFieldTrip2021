946c2cefdbd4eaf30814a6011ee5486cfcf17c14
Brian Jackson <decaelus@gmail.com>
2021-07-09 19:21:15 -0600

    Added some code to process Kestrel data

---
+++ code/Kestrel_analysis/utils.py
@@ -0,0 +1,14 @@
+def read_kestrel_data(filename):
+    names_to_use = ['DateTime','Temp','Wet_Bulb_Temp','Rel_Hum','Baro','Altitude','Station_P',
+                    'Wind_Speed','Heat_Index','Dew_Point','Dens_Alt','Crosswind','Headwind','Mag_Dir',
+                    'True_Dir','Wind_Chill']
+    data = np.genfromtxt(filename, delimiter=',', skip_header=11, 
+                         names=names_to_use, 
+                         dtype=None)
+
+    # Convert first two columns into datetime
+    datatime = pd.to_datetime(data['DateTime'].astype(str))
+    dataframe = pd.DataFrame(data[names_to_use[1:]])
+    dataframe.insert(0, 'DateTime', datatime) 
+    
+    return dataframe
